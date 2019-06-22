85a628f27ad221055189fe48fefb52ac62ab34be
asternaz22 <48803751+asternaz22@users.noreply.github.com>
2019-06-22 16:43:53 +0500

    Add files via upload

---
+++ aster.js
@@ -0,0 +1,172 @@
+ 
+/* let age = 17;
+ let hasProgrammingKnow = true; 
+
+if (age > 15 && !hasProgrammingKnow) {
+  alert("Eligible");
+}
+else{
+  alert("Not");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let personHasRs = 3000000;
+if (personHasRs >= 1000000){
+  alert("Congratulatoin you are millionaire");
+}
+else{
+  alert("I'm sorry you are not millionaire");
+}
+
+
+
+
+
+
+
+
+
+
+
+let isElectric = true;
+if (isElectric){
+  alert("your car is manufactured by Tesla");
+}
+else{
+  alert("your car is not manufactured by Tesla");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+
+let highScore = 20;
+let currentScore = 10;
+let needScore = currentScore - highScore;
+if (highScore <= currentScore){
+  alert("Congratulations!! Your new high score ");
+}
+else{
+  alert("Try Again. You just need" + needScore);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let number = 4;
+if (numberx%2 ===0){
+  alert("this is even");
+}
+else{
+  alert("this is odd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let balance = -10;
+if (balance > 1){
+  alert("Moazziz Sarif, aap ka balance khatam honay wala hai plz recharge kren");
+}
+else if(balance <= 10){
+  alert("Moazziz Sarif, apka balance nakafi hai");
+  alert("Ring Ring");
+}
+else{
+  alert("Ring Ring");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let math = 50;
+let english = 50;
+let computer = 50;
+if (math > 10 || english > 8 || computer > 20){
+  alert("passed");
+}
+else{
+  alert("failed");
+}
+*/
+
+
+
+
+
+
+
+
+
+const costOfApple = 10;
+const costOfBanana = 8;
+const costOfMango = 15;
+
+let noOfApple = 1;
+let noOfBanana = 2;
+let noOfMango = 3;
+const aliBalance = 100;
+let totalCost =  noOfApple *costOfApple + noOfBanana * costOfBanana  + noOfMango * 
+
